3cd0c68e85168c9f7bbe8303b714c8636b32c692
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-01-30 13:51:52 -0500

    changed directory name

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ import setuptools
 
 setuptools.setup(
     name='ethiopian-date-converter',
-    version=__import__('ethiopian_date').__version__,
+    version=__import__('ethiopian_date_converter').__version__,
     license='GNU General Public License (GPL), Version 3',
 
-    provides=['ethiopian_date'],
+    provides=['ethiopian_date_converter'],
 
     description='Ethiopian date converter.',
     long_description=open('README.rst').read(),
 
     url='https://github.com/dimagi/ethiopian-date-converter',
 
-    packages=['ethiopian_date'],
+    packages=['ethiopian_date_converter'],
 
     classifiers=[
         'Development Status :: 4 - Beta',
