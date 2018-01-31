b6e5a8342f82d4ef74a2e4711fe50938e29154cb
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-01-31 14:39:14 -0500

    added a print stmt

--- ethiopian_date/ethiopian_date.py
+++ ethiopian_date/ethiopian_date.py
@@ -222,5 +222,6 @@ class EthiopianDateConverter(object):
         # Ethiopian months ordered according to Gregorian
         order = [0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4]
         ethiopian_month = order[m]
+        print("PREETHI: New package reached")
 
         return ethiopian_year, ethiopian_month, ethiopian_date
